573ae8bccf0aee44b74c5c685fb59ded3b434798
maheshsingh20 <singhmahesh2924@gmail.com>
2024-10-16 12:22:40 +0530

    commit1

---
+++ st.php
@@ -0,0 +1,11 @@
+<?php
+  // define two numbers
+  $num1 = 5;
+  $num2 = 10;
+
+  // calculate the sum
+  $sum = $num1 + $num2;
+
+  // print the result
+  echo "The sum of $num1 and $num2 is: $sum";
+?>
